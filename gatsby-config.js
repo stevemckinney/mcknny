5c570895f91114de16336a892122507d2f73a941
stevemckinney <steve@iamsteve.me>
2020-04-15 16:48:32 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,31 +5,61 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
-    description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`,
+    title: `steve mckinney`,
+    short_name: `sm`,
+    description: `Portfolio`,
+    author: `@irsteve`,
+    "navigation": [
+      {
+        "name": "home",
+        "link": "/"
+      },
+      {
+        "name": "portfolio",
+        "link": "/portfolio"
+      },
+      {
+        "name": "about",
+        "link": "/about"
+      },
+      {
+        "name": "contact",
+        "link": "/contact"
+      }
+    ]
   },
   plugins: [
+    `gatsby-plugin-offline`,
     `gatsby-plugin-eslint`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
+        name: `steve mckinney`,
+        short_name: `sm`,
         start_url: `/`,
         background_color: fullConfig.theme.colors.white,
-        theme_color: fullConfig.theme.colors.teal["400"],
+        theme_color: fullConfig.theme.colors.spruce,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`,
+        icon: `src/images/apple-touch-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        siteUrl: `https://stevemckinney.net`,
       },
     },
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
         postCssPlugins: [
+          require("postcss-import"),
           require(`tailwindcss`)(tailwindConfig),
           require(`autoprefixer`),
+          require(`postcss-preset-env`)({ stage: 0 }),
           ...(process.env.NODE_ENV === `production`
             ? [require(`cssnano`)]
             : []),
@@ -40,9 +70,16 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         tailwind: true,
-        purgeOnly: [`src/css/style.css`],
+        purgeOnly: [`src/css/global.css`],
       },
     },
-    `gatsby-plugin-offline`,
+    {
+     resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        typekit: {
+          id: 'cbu1mww'
+        }
+      } 
+    }
   ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,56 @@
 // See https://tailwindcss.com/docs/configuration for details
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  theme: {},
-  variants: {},
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['alverata', 'serif'],
+      body: ['gira-sans', 'sans-serif'],
+    },
+    colors: {
+      // General
+      black: colors.black,
+      white: colors.white,
+      
+      // Primary
+      madison: 'var(--color-madison)',
+      spruce: 'var(--color-spruce)',
+      gold: 'var(--color-gold)',
+      
+      // Secondary
+      feldspar: 'var(--color-feldspar)',
+      seagull: 'var(--color-seagull)',
+      thulian: 'var(--color-thulian)',
+      
+      // Shades & text
+      blanc: 'var(--color-blanc)',
+      sidecar: 'var(--color-sidecar)'
+    },
+    fontSize: {
+      'f9': '10px',
+      'f8': '12px',
+      'f7': '14px',
+      'f6': '16px',
+      'f5': '18px',
+      'f4': '21px',
+      'f3': '24px',
+      'f2': '32px',
+      'f1': '40px',
+      'headline': '48px'
+    },
+    extend: {},
+  },
+  variants: {
+    spacing: ['responsive'],
+    fontSize: ['responsive']
+  },
+  corePlugins: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
 };
