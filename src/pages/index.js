adc7d37647577476b49a22695e4e972676a2eb59
stevemckinney <steve@iamsteve.me>
2020-04-16 21:11:44 +0100

    conveniences

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-//import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 function IndexPage() {
   return (
     <Layout>
       <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="Home"
+        title="steve mckinney ux & visual design"
       />
 
       <section className="text-center">
         <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
           Hey there! Welcome to your first Gatsby site.
         </h2>
+        <Link to="/about">About</Link>
       </section>
     </Layout>
   );
