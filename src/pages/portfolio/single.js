7954d9005772209654aa5ee927a52a064b0fbde2
stevemckinney <mail@iamsteve.me>
2020-04-24 10:22:26 +0100

    portfolio bits

---
+++ src/pages/portfolio/single.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Hero from "../components/hero";
+
+function Single() {
+  return (
+    <Layout>
+      <SEO title="portfolio item" />
+      
+      <h1>portfolio item</h1>
+    </Layout>
+  )
+}
+
+export default Single;
