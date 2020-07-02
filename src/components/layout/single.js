b06e7fd019e9bdaa5059f497fdc6416ac066d63c
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2020-07-02 19:12:39 +0100

    make portfolio images size more appropriately

--- src/components/layout/single.js
+++ src/components/layout/single.js
@@ -36,6 +36,9 @@ export default function Single({ data: { mdx } }) {
     title,
     description } = mdx.frontmatter;
 
+  const headingStyles = `relative z-10 mb-2 md:mb-6 text-f3 xxs:text-f2 sm:text-f1 xxl:text-headline tracking-tight sm:tracking-tighter xxl:tracking-headline`;
+  const titleStyles = `mb-4 text-f8 text-melon uppercase tracking-title font-title font-bold`;
+
   function createSeparator() {
     return {__html: '/'};
   }
@@ -43,12 +46,20 @@ export default function Single({ data: { mdx } }) {
   return (
     <Layout>
       <SEO title={title}/>
-      <header className="grid layout full screen screen-rows bg-i relative">
-        <div className={`${styles.content} screen-center col-span-5 col-start-4`}>
-          <a href={url} className="mb-4 text-f8 text-melon uppercase tracking-title font-title font-bold">{url.replace("https://", "")}</a>
-          <h1 className="relative z-10 mb-6 mdMax:leading-tight text-f3 lg:text-headline tracking-headline">{description}</h1>
+      <header className="grid layout full screen xxl:screen-rows bg-i relative">
+        <div
+          className={`
+            content xxl:screen-center xl:col-span-4 xl:col-start-3 xxxl:col-span-5 xxxl:col-start-4
+        `}>
+          {url ?
+            <a href={url} className={titleStyles}>{title}</a> :
+            <span className={titleStyles}>{title}</span>
+          }
+          <h1 className={headingStyles}>{description}</h1>
         </div>
-        <div className="screen-center col-span-7">
+        <div className={`
+          content xxl:screen-center xxxl:col-start-10 xl:col-span-6
+        `}>
           <ul className={`m-0 p-0 flex mb-4 text-f8 text-pearl uppercase tracking-title font-title font-bold gap-4`}>
             <li className="text-melon list-item">Role</li>
             {role.map((name, i) => [
