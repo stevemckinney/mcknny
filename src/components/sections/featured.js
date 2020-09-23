a9bcfadcadace7d821bfd840d022a40f91112f50
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2020-09-23 17:24:49 +0100

    refinements

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -13,22 +13,24 @@ const Featured = (props) => {
     <React.Fragment>
       <section className={props.className}>
         <header className="flex justify-between mb-12 col-span-12">
-          <h2 className="text-f2 md:text-f1 text-melon content mb-0 content columns-all">Projects</h2>
+          <h2 className="text-f2 md:text-f1 text-melon content mb-0 content columns-all">
+            Projects
+          </h2>
           <Action to="/work/" text="View all projects" />
         </header>
 
         <Projects />
       </section>
     </React.Fragment>
-  )
-}
+  );
+};
 
 Featured.defaultProps = {
-  className: "featured content md:grid gap-8 lg:gap-16 mdMax:pt-36",
-}
+  className: "featured content md:grid gap-8 mdMax:pt-36",
+};
 
 Featured.propTypes = {
   className: PropTypes.node,
-}
+};
 
 export default Featured;
