7b3d26272505356258bbdfbad491eb8a12bb6b4a
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2020-09-24 19:04:04 +0100

    move to the root of the project with all other similar files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Layout from "./components/layout";
+import Layout from "./src/components/layout";
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
