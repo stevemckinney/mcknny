720ca0d8c51e5b3f4c7b59810abdbea618bcb6ab
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2020-09-29 14:11:04 +0100

    responsive design refinements

--- src/components/sections/profile.js
+++ src/components/sections/profile.js
@@ -29,14 +29,14 @@ const Profile = (props) => {
         <div className={contentStyles}>
           <Logo className="pb-8" />
           <h2 className={`${styles.title} text-f3 xxs:text-f2 md:text-f1 tracking-tight sm:tracking-tighter text-remy content mb-12`} dangerouslySetInnerHTML={setTitle()} />
-          <h3 className="block text-f7 md:text-f6 text-pearl uppercase tracking-title font-title font-medium mb-4">Let’s talk design</h3>
+          <h3 className="block text-f7 md:text-f5 text-pearl uppercase tracking-title font-title font-medium mb-4">Let’s talk design</h3>
           <ul className="flex mdMax:flex-col font-bold text-f6 md:text-f5 p-0 m-0">
             <li className="pr-8">
               <a href="mailto:hi@mcknny.com" className="text-jasmine hover:text-sidecar">Email</a>
             </li>
-            {/*<li className="pr-8">
+            {<li className="pr-8">
               <a href="https://instagram.com/studiomcknny" className="text-jasmine hover:text-sidecar">Instagram</a>
-            </li>*/}
+            </li>}
             <li className="pr-8">
               <a href="https://www.behance.net/stevemckinney" className="text-jasmine hover:text-sidecar">Behance</a>
             </li>
