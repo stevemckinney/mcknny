0e640544b4ba0ff51f2cfd470288d83b277b3ab9
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2020-10-10 10:17:41 +0100

    more and more refinements

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -9,7 +9,7 @@ const WorkPage = () => {
       <header className="py-64 lg:mt-16 content">
         <h1 className="relative z-10 mb-0 text-f2 xs:text-f1 xxl:text-headline">Work</h1>
       </header>
-      <section className={`grid content grid-cols-10 col-gap-16 row-gap-0`}>
+      <section className={`grid content md:grid-cols-10 col-gap-16 row-gap-0`}>
         <Projects />
       </section>
     </React.Fragment>
