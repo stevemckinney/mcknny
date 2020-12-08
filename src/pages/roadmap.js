6f3c119ceeb9d89ea23a171558fd3860cfc282f3
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2020-12-08 07:38:54 +0000

    change designing in the open to roadmap

--- src/pages/roadmap.js
+++ src/pages/roadmap.js
@@ -16,17 +16,38 @@ function DesigningPage() {
           <p>This page like much of this website is currently a work in progress. For anyone stumbling across this portfolio, they will likely see a largely complete design. But may find bugs and a lot incomplete bits of content.</p>
           <p>It’s not a great reflection—but a busy life leads to slow but steady progress. Over the coming <em>months</em>, I hope to complete more portfolio & content pages, refine the poorer parts of the design and layouts.</p>
           <p>Overall leading to a complete portfolio, with messaging that reflects my skill & ability to deliver you a wonderful website, that serves your customers.</p>
-          <h2 className="text-melon mt-8">Task list</h2>
-          <ul>
-            <li>Complete the task list</li>
-            <li>Make this page more readable</li>
-            <li>Refine the responsive layout on <Link to="/work/" className="text-jasmine hover:text-sidecar">work</Link> at small screen sizes</li>
-            <li>Complete the about page</li>
-            <li>Complete the process page</li>
-            <li>Complete the contact page</li>
-            <li>Complete 8 portfolio case studies</li>
-          </ul>
         </div>
+        <h2 className="text-melon mt-8">Tasks</h2>
+        <ul>
+          <li>
+            <h3>General</h3>
+            <ul>
+              <li>Complete the task list</li>
+              <li>Make this page more readable</li>
+              <li>Improve site performance</li>
+              <li>Complete alt & figcaptions where necessary</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Content</h3>
+            <ul>
+              <li>Complete 8 portfolio case studies</li>
+              <li>Choose the final 3 case studies</li>
+              <li>Create content for each portfolio</li>
+              <li>Create images for each portfolio section</li>
+              <li>Complete the about page</li>
+              <li>Complete the process page</li>
+              <li>Complete the contact page</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Design</h3>
+            <ul>
+              <li>Refine the responsive layout on <Link to="/work/" className="text-jasmine hover:text-sidecar">work</Link> at small screen sizes</li>
+              <li>Figure out how to add in better page transitions and animations</li>
+            </ul>
+          </li>
+        </ul>
       </article>
     </React.Fragment>
   );
