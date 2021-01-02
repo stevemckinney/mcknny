a604365b538b3169f0262515e19fc5bbe572172c
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-01-02 20:24:24 +0000

    additions to avoid being purged

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,7 +117,8 @@ module.exports = {
           `opacity-40`,
           `grid-cols-10`,
           `image`,
-          ``,
+          `figure`,
+          `img`
         ],
         content: [
           path.join(process.cwd(), `content/**/!(*.d).{js,jsx,md,mdx,svg}`),
