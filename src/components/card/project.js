c5a674a67010cc92765dc3cc00c0da764ec4bf0e
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-01-13 20:40:30 +0000

    add motion to projects

--- src/components/card/project.js
+++ src/components/card/project.js
@@ -2,28 +2,68 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import Img from "gatsby-image";
+import { motion } from "framer-motion";
 //import Dump from "@components/dump";
 
 // css
 import styles from "./project.module.css";
 
+// motion
+const text = {
+  transition: {
+    type: "tween",
+    ease: "easeInOut"
+  },
+  initial: {
+    duration: .8,
+    y: 0,
+  },
+  hover: {
+    duration: .8,
+    y: -48
+  }
+}
+
+const image = {
+  transition: {
+    type: "tween",
+    ease: "easeInOut"
+  },
+  initial: {
+    duration: .4,
+    y: 0,
+    scale: 1,
+  },
+  hover: {
+    duration: .4,
+    scale: .9,
+  }
+}
+
 const Project = ({ frontmatter, className, link }) => {
   return (
     <React.Fragment>
       {/*<Dump individualProjectProps={frontmatter} />*/}
-      <article className={`${styles.container} ${className}`}>
-        <Link to={link} className="block">
-          <Img fluid={frontmatter.image.childImageSharp.fluid} />
-        </Link>
-        <div className="meta px-8 py-8 lg:px-20 xl:py-16 xl:px-24 xxl:px-16 text-center">
+      <motion.article 
+        initial="initial"
+        whileHover="hover"
+        animate="initial"
+        className={`${styles.container} ${className}`}
+      >
+        <motion.div variants={image}>
+          <Link to={link} className="block">
+            <Img fluid={frontmatter.image.childImageSharp.fluid} />
+          </Link>
+        </motion.div>
+        <motion.div variants={text} className="meta px-8 py-8 lg:px-20 xl:py-12 xl:px-24 xxl:px-16 text-center">
           <p className="block text-f7 md:text-f6 lg:text-f5 text-patina font-medium font-title lowercase mb-1">{frontmatter.title}</p>
           <h2 className="font-headline text-f3 lg:text-f2 m-0 w-11/12 mx-auto">
             <Link to={link} className="block">
               {frontmatter.description}
             </Link>
           </h2>
-        </div>
-      </article>
+        </motion.div>
+      </motion.article>
     </React.Fragment>
   )
 }
