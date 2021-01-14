5752c2e7e1377120cf0463f9a91daad13983d950
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-01-14 08:13:06 +0000

    missing an updated title tag

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -6,6 +6,7 @@ import Projects from "@components/content/projects";
 const WorkPage = () => {
   return (
     <React.Fragment>
+      <SEO title="Work" />
       <header className="py-64 lg:mt-16 content">
         <h1 className="relative z-10 mb-0 text-f2 xs:text-f1 xxl:text-headline">Work</h1>
       </header>
